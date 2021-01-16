a8f81ff205dc19bd0e6d1e9ef6a83a1bed0e55ec
zuramai <ahmadsaugigis@yahoo.com>
2021-01-16 21:21:54 +0700

    :rocket: first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+func main() {
+	firmataAdaptor := firmata.NewAdaptor("COM3")
+	led := gpio.NewLedDriver(firmataAdaptor, "7")
+
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
+
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	)
+
+	robot.Start()
+}
